d69336f409561c98411e62b70212561da1ad7a51
Ethan Olsen <131eolsen@gmail.com>
2024-06-22 18:55:43 -0700

    feat: add astro db

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import svelte from "@astrojs/svelte";
-
 import tailwind from "@astrojs/tailwind";
+
+import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind()]
+  integrations: [svelte(), tailwind(), db()]
 });
